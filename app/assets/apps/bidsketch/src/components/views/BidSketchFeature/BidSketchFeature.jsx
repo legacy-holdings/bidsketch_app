fce1a28a38e5900bc114f360691b963d35e53064
ahstudybank <alexanderpholland@Alexanders-MacBook-Air.local>
2017-07-04 23:32:04 -0400

    ba: Builds core UI
    This commit focuses on building the necessary UI
    components and sets up local state to handle user flow.
    Connecting to redux next.

--- app/assets/apps/bidsketch/src/components/views/BidSketchFeature/BidSketchFeature.jsx
+++ app/assets/apps/bidsketch/src/components/views/BidSketchFeature/BidSketchFeature.jsx
@@ -1,25 +1,28 @@
+// Global Imports
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { Grid, Row, Col } from "react-bootstrap";
+import { Grid, Row, Col, Tooltip } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import $ from "jquery";
 
+// Material UI Imports
 import ChevronLeft from "material-ui/svg-icons/navigation/chevron-left";
+import ChevronRight from "material-ui/svg-icons/navigation/chevron-right";
 import IconButton from "material-ui/IconButton";
 import RaisedButton from "material-ui/RaisedButton";
 import CircularProgress from "material-ui/CircularProgress";
+import Checkbox from 'material-ui/Checkbox';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
+// Local Imports
 import * as bidSketchFeatureActions
   from "../../../actions/bidsketch-feature-actions.js";
-
 import ProgressBarStepper
   from "./common/ProgressBarStepper/ProgressBarStepper.jsx";
-
-import $ from "jquery";
-
 import DocumentItem from "./DocumentItem.jsx";
 import DocumentItemsCarousel from "./DocumentItemsCarousel.jsx";
-
 import "./css/BidSketchFeature.css";
 
 // Icon/Button Styles --------------------------
@@ -42,6 +45,51 @@ const backToHomeStyle = {
 };
 // ---------------------------------------------
 
+/**
+ * A modal dialog can only be closed by selecting one of the actions.
+ */
+class CompletedDialog extends Component {
+    constructor() {
+        super();
+        this.state = {
+            open: true,
+        };
+    }
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
+  render() {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        disabled={true}
+        onTouchTap={this.handleClose}
+      />,
+    ];
+
+    return (
+      <div>
+        <Dialog
+          title="Dialog With Actions"
+          actions={actions}
+          modal={true}
+          open={this.state.open}
+        >
+          Only actions can close this dialog.
+        </Dialog>
+      </div>
+    );
+  }
+}
+
 class BidSketchFeature extends Component {
     constructor() {
         super();
@@ -55,11 +103,13 @@ class BidSketchFeature extends Component {
                 <DocumentItem isActive={false} isDisabled={true} />,
                 <DocumentItem isActive={false} isDisabled={true} />,
                 <DocumentItem isActive={false} isDisabled={true} />,
-                <DocumentItem isActive={false} isDisabled={true} />
             ],
             documentItemsCarousel: false,
             viewLoading: true,
-            activeDocument: undefined // undefined - until "Click to START" btn triggered, integer - represents which document is currently active
+            activeDocument: undefined, // undefined - until "Click to START" btn triggered, integer - represents which document is currently active,
+            reactChecked: undefined,
+            goChecked: undefined,
+            completedDialogView: ""
         };
 
         this.loadingView = undefined;
@@ -85,7 +135,6 @@ class BidSketchFeature extends Component {
                           <DocumentItem isActive={false} isDisabled={false} />,
                           <DocumentItem isActive={false} isDisabled={false} />,
                           <DocumentItem isActive={false} isDisabled={false} />,
-                          <DocumentItem isActive={false} isDisabled={false} />
                       ],
                       documentItemsCarousel: true
                   },
@@ -115,7 +164,6 @@ class BidSketchFeature extends Component {
                           <DocumentItem isActive={false} isDisabled={false} />,
                           <DocumentItem isActive={false} isDisabled={false} />,
                           <DocumentItem isActive={false} isDisabled={false} />,
-                          <DocumentItem isActive={false} isDisabled={false} />
                       ],
                       documentItemsCarousel: false
                   },
@@ -132,11 +180,41 @@ class BidSketchFeature extends Component {
             }
         }, 1000);
     }
-
+    
     handleClickStart() {
         this.setState({
             showClickToStartBtn: false
         });
+    }
+
+    handleReactCheck() {
+        if (!this.state.reactChecked) {
+            this.setState({
+                reactChecked: true
+            });
+        } else {
+            this.setState({
+                reactChecked: false
+            });
+        }
+    }
+
+    handleGoCheck() {
+        if (!this.state.goChecked) {
+            this.setState({
+                goChecked: true
+            });
+        } else {
+            this.setState({
+                goChecked: false
+            });
+        }
+    }
+    
+    handleCompleteClick() {
+        this.setState({
+            completedDialogView: <CompletedDialog />
+        })
     }
 
     render() {
@@ -148,17 +226,46 @@ class BidSketchFeature extends Component {
           className="bidsketch-btn"
           onClick={() => this.handleClickStart()}
         >
-          <div className="document-nav-btn-text">Click to Start</div>
+          <div className="document-nav-btn-text">Click to START</div>
         </RaisedButton>
       : <div />;
 
         let overlayClass = "";
+        let disableFirstCheckBox = true;
+        let disableSecondCheckBox = true;
+        let disableCompleteButton = true;
+        let firstCheckBoxTooltip = "";
+        let secondCheckBoxTooltip = "";
+        let firstCheckBoxClass = "";
+        let secondCheckBoxClass = "";
+        
+        
         if (this.state.showClickToStartBtn) {
-      // Disable document
-      // Create css dimmer out overlay
             overlayClass = "disabled-overlay";
+            
         } else {
             overlayClass = "overlay-container";
+            disableFirstCheckBox = false;
+            disableSecondCheckBox = false;
+            disableCompleteButton = false;
+
+            if (this.state.reactChecked === undefined) {
+                firstCheckBoxTooltip = <div id="fade-first-checkbox">
+                                <Tooltip placement="top" className="in" id="click-to-check-first-tooltip">
+                                    Click to CHECK
+                                </Tooltip>
+                            </div>;    
+                firstCheckBoxClass = "first-checkbox";                
+            }
+
+            if (this.state.goChecked === undefined) {
+                secondCheckBoxTooltip = <div id="fade-second-checkbox">
+                            <Tooltip placement="bottom" className="in" id="click-to-check-second-tooltip">
+                                Click to CHECK
+                            </Tooltip>
+                        </div>;           
+                secondCheckBoxClass = "second-checkbox";
+            }
 
             this.state.documentItems = [
                 <DocumentItem isDisabled={false} />,
@@ -166,7 +273,6 @@ class BidSketchFeature extends Component {
                 <DocumentItem isDisabled={false} />,
                 <DocumentItem isDisabled={false} />,
                 <DocumentItem isDisabled={false} />,
-                <DocumentItem isDisabled={false} />
             ];
         }
 
@@ -188,8 +294,43 @@ class BidSketchFeature extends Component {
           <Col xs={12} sm={2} md={2} lg={2} className="document-pages-column">
             {documentItemsView}
           </Col>
-          <Col xs={12} sm={10} md={10} lg={10} className="center">
-            Text
+          <Col xs={12} sm={10} md={10} lg={10} className="checkbox-form-container center">
+            <Row className="checkbox-row">
+                <Col xs={8} xsOffset={2} sm={4} smOffset={4} md={4} mdOffset={4}>
+                    {firstCheckBoxTooltip}
+                    <Checkbox
+                        onCheck={() => this.handleReactCheck()}
+                        className={firstCheckBoxClass}
+                        label="Do you like React?"
+                        disabled={disableFirstCheckBox}
+                    />
+                </Col>
+            </Row>
+            <Row className="checkbox-row">
+                <Col xs={8} xsOffset={2} sm={4} smOffset={4} md={4} mdOffset={4}>
+                    {secondCheckBoxTooltip}
+                    <Checkbox
+                        onCheck={() => this.handleGoCheck()}
+                        className={secondCheckBoxClass}                        
+                        label="What about Go?"
+                        disabled={disableSecondCheckBox}
+                    />
+                </Col>
+            </Row>
+            <Row className="checkbox-row">
+                <Col xs={8} xsOffset={2} sm={4} smOffset={4} md={4} mdOffset={4}>
+                        {this.state.completedDialogView}
+                        <RaisedButton
+                            onClick={() => this.handleCompleteClick()}
+                            style={{ marginLeft: "60px", marginTop: "50px"  }}
+                            labelPosition="before"
+                            primary={true}
+                            disabled={disableCompleteButton}
+                            label="Complete"
+                            icon={<ChevronRight />}
+                        />
+                </Col>
+            </Row>
           </Col>
         </div>;
 
@@ -218,6 +359,7 @@ class BidSketchFeature extends Component {
             </div>
           </Col>
         </Row>
+        {/* Animate pages: https://github.com/szchenghuang/react-transitions#transitions */}
         <div className={overlayClass}>
           <Row className={`document-builder-container ${overlayClass}`}>
             {documentBuilderView}
